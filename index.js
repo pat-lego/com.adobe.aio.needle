1c06bdc54b22719e78219bfa8d237a8fd21ca587
Patrique Legault <patrique.legault@gmail.com>
2025-09-24 14:16:41 -0400

    Adding some stuff

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var needle = require('needle');
+const httpsProxyAgent = require('https-proxy-agent')
+
+const agent = new httpsProxyAgent.HttpsProxyAgent('')
 
 needle.get('https://deploy-service.app-builder.adp.adobe.io/runtime/api/v1/namespaces/16225-665azurecat-stage/packages?', {
     accept: 'application/json',
@@ -7,14 +10,20 @@ needle.get('https://deploy-service.app-builder.adp.adobe.io/runtime/api/v1/names
         'authorization': 'Bearer ',
         'x-namespace-id': '16225-665azurecat-stage'
     },
+    agent: agent,
     connection: 'keep-alive'
-}, function(err, resp) {
+}, function (err, resp) {
     if (err) {
         console.error("Received error", err)
         return;
     }
 
-    console.info({'code' :resp.statusCode})
-    console.info({'body': resp.body})
-    console.info({'headers': resp.headers})
+    console.info({ 'code': resp.statusCode })
+    console.info({ 'body': resp.body })
+
+    if (resp.statusCode == 400) {
+        const responseBody = resp.body.toString('utf8');
+        console.log(responseBody);
+    }
+    console.info({ 'headers': resp.headers })
 })
